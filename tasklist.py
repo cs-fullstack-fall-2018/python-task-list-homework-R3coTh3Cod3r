891cfabc02bc9a12d5643b3523d4ac74a0ee42eb
Mareco Nabors <naborsmarico99@gmail.com>
2018-07-18 15:41:55 -0500

    tasklist

---
+++ tasklist.py
@@ -0,0 +1,15 @@
+
+
+
+def thingsToDo():
+
+    print("Congrationlations!", "You're running Reco's Task List program.")
+    print("What would you like do next?")
+    print("List all tasks.")
+    print("Add a task to the list.")
+    print("Delete a task.")
+
+thingsToDo()
+
+
+
